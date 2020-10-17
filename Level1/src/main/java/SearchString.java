85f7a0546dbd2f6aa065907088b3e9bcff988f6b
YCHULHEE <ych9411@naver.com>
2020-10-18 04:25:45 +0900

    SearchString Update.

---
+++ Level1/src/main/java/SearchString.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class SearchString {
+    public String solution(String[] seoul) {
+        String answer = "";
+        for (int i = 0; i < seoul.length; i++){
+            if (seoul[i].equals("Kim")) {
+                answer += "김서방은" + i + "에 있다";
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
