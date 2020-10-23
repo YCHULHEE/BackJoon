7358e3cedbe18a493b2b6c5c1b5c0f53baf21e9c
YCHULHEE <ych9411@naver.com>
2020-10-24 04:50:15 +0900

    Budget Update.

---
+++ Level1Page2/src/main/java/Budget.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Budget {
+    public int solution(int[] d, int budget) {
+        int answer = 0;
+
+        Arrays.sort(d);
+
+        for (int i = 0; i < d.length; i++) {
+            if ((budget - d[i]) < 0) {
+                return answer;
+            }
+
+            if (budget > 0) {
+                budget -= d[i];
+                answer++;
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
