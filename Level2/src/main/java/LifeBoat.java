23f83b677d1be3bdddc84cd4aca1d5294353e8c2
YCHULHEE <ych9411@naver.com>
2020-11-01 21:51:59 +0900

    LifeBoat Update.

---
+++ Level2/src/main/java/LifeBoat.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class LifeBoat {
+    public static void main(String[] args) {
+    int[] people = {70, 80, 50};
+    int limit = 100;
+    int a = solution(people, limit);
+
+    }
+
+    public static int solution(int[] people, int limit) {
+        int answer = 0;
+
+        Arrays.sort(people);
+
+        int min = 0;
+
+        for (int max = people.length - 1; min <= max; max--){
+            if (people[min] + people[max] <= limit) min++;
+            answer++;
+        }
+
+        return answer;
+    }
+}
+
+
+
+class Boat {
+    int peopleWeight;
+
+    Boat(int people, int limit) {
+        this.peopleWeight = people;
+    }
+}
+
+
